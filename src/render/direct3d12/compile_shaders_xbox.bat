3ae570f547967255cccaf7ec5ae54820cb69231c
Andreia Gaita <shana@spoiledcat.net>
2026-09-17 14:34:43 +0100

    Fix SDL Xbox vcxproj so it works with GDK 20260403

--- src/render/direct3d12/compile_shaders_xbox.bat
+++ src/render/direct3d12/compile_shaders_xbox.bat
@@ -1,6 +1,7 @@
 if %2.==one. goto setxboxone
 rem Xbox Series compile
 set DXC="%GameDKLatest%\GXDK\bin\Scarlett\DXC.exe"
+if not exist $DXC set DXC="%GameDKXboxLatest%\xbox\bin\gen9\DXC.exe"
 set SUFFIX=_Series.h
 goto startbuild
 
